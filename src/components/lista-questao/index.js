73e6ecdaf68fb0c860bfcac6e00a24de99dff7c4
Vinicius <vinicius.cross07@gmail.com>
2019-10-11 01:34:41 -0300

    excluir

--- src/components/lista-questao/index.js
+++ src/components/lista-questao/index.js
@@ -1,9 +1,18 @@
 import React, { useState , useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './lista-questao.css';
+import firebase from '../../config/firebase'
 
 
-function Listagem({id,enunciado,altCerta,autor}){
+function Listagem({id,enunciado,altCerta,autor,cursoCol}){
+
+  function deletarQuestao(){
+    firebase.firestore().collection(cursoCol).doc(id).delete().then(function() {
+      alert("Questão deletada com sucesso!");
+  }).catch(function(error) {
+      console.error("Error ao remover:\n ", error);
+  });
+  }
 
     return(
         <div className='m-3'>
@@ -26,7 +35,7 @@ function Listagem({id,enunciado,altCerta,autor}){
                 <td>{autor}</td>
                 <td>
                   <button type='button' className='btn btn-primary mr-1'>Editar</button>
-                  <button type='button' className='btn btn-danger ml-1'>Excluir</button>
+                  <button type='button' className='btn btn-danger ml-1' onClick={deletarQuestao}>Excluir</button>
                 </td>
               </tr>
             </tbody>
